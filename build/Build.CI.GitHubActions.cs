8258eb4e17a07868f52a250ebf5fee89edf34861
Ebere Abanonu <eaba@users.noreply.github.com>
2022-07-16 01:51:53 +0100

    All

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -27,7 +27,7 @@ using Nuke.Common.Utilities;
     InvokedTargets = new[] { nameof(NuGet) },
     ImportSecrets = new[] { "Nuget_Key"},
     PublishArtifacts = true,
-    EnableGitHubToken = true)//
+    EnableGitHubToken = true)//=====
     ////////////
 ]
 
